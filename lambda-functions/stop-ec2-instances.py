b3cb5ced8d6a7edb163b5be36c7b143ff18fd8fb
chandranathmondal <42894367+chandranathmondal@users.noreply.github.com>
2019-12-11 14:27:46 +0530

    Create stop-ec2-instances.py

---
+++ lambda-functions/stop-ec2-instances.py
@@ -0,0 +1,18 @@
+import json
+import os
+import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    instance_ids = event['InstanceIds']
+    ec2.stop_instances(InstanceIds=instance_ids)
+    logger.info('Stopped instance(s): ' + str(instance_ids))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Stopped instance(s): ' + str(instance_ids))
+    }
